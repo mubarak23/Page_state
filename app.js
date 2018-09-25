2ef882ce17b5b0b85316fdf9a5ce04162aa3f49e
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-09-25 11:12:20 +0100

    home state setup successfully

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ const PageState = function() {
   this.init = function() {
     this.change(new homeState());
   };
+
+  this.change = function(state) {
+    currentState = state;
+  };
+};
+
+//home state
+const homeState = function() {
+  document.querySelector('#heading').textContent = null;
+  document.querySelector('#content').innerHTML = `
+      <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  `;
 };
